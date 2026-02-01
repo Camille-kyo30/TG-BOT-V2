cd3709612ab7df51fe2569be130b0b4dad1c0785
Camille-kyo30 <gnandekouakou710@gmail.com>
2026-02-01 16:25:36 +0000

    Create redémarrer.js

---
+++ scripts/cmds/redémarrer.js
@@ -0,0 +1,19 @@
+const nix = {
+  name: "redémarrer",
+  version: "1.0.0",
+  aliases: ["restart"],
+  description: "Redémarre le bot",
+  author: "Gnande",
+  prefix: true,
+  category: "system",
+  type: "admin",
+  cooldown: 5,
+  guide: "{pn}"
+};
+
+async function onStart({ message }) {
+  message.reply("🔄 Redémarrage en cours...");
+  process.exit(1);
+}
+
+module.exports = { nix, onStart };
